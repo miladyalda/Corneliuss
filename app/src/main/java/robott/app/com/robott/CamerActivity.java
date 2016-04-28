c53d0688f68fd2351d8e51c3d7a215153e43480a
milad yalda <milad_gbg@hotmail.com>
2016-04-28 09:53:53 +0200

    tiltpan joystick and camera stream

--- app/src/main/java/robott/app/com/robott/CamerActivity.java
+++ app/src/main/java/robott/app/com/robott/CamerActivity.java
@@ -1,6 +1,9 @@
 package robott.app.com.robott;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.provider.Settings;
@@ -8,32 +11,58 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.MediaController;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.VideoView;
 
-public class CamerActivity extends AppCompatActivity {
+import java.io.IOException;
+
+public class CamerActivity extends AppCompatActivity implements SurfaceHolder.Callback {
 
 
     RelativeLayout layout_joystick;
 
+    RelativeLayout layout_joystickPantilt;
+
     ImageView image_joystick, image_border;
 
     TextView textView1, textView2, textView3, textView4, textView5;
-    JoyStickClass js;
 
+    TextView textViewPan1, textViewPan2, textViewPan3, textViewPan4, textViewPan5;
+    JoyStickClass js;
+    JoystickClassPanTilt jsPanTilt;
+
+    MediaPlayer mp;
 
 
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camer);
 
         startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
 
+        final VideoView myVideoView = (VideoView)findViewById(R.id.videoView);
 
+       // rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov
+        String videoSource = "rtsp://192.168.42.1:8001";
+
+        myVideoView.setMediaController(new MediaController(this));
+        myVideoView.setVideoURI(Uri.parse(videoSource));
+        myVideoView.requestFocus();
+        myVideoView.start();
+
+
+
+        //String mediaURL = "rtsp://192.168.42.1:8001";
+        //Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(mediaURL));
+        //startActivity(intent);
 
         textView1 = (TextView)findViewById(R.id.textView1);
 
@@ -46,12 +75,34 @@ public class CamerActivity extends AppCompatActivity {
         textView5 = (TextView)findViewById(R.id.textView5);
 
 
+
+
+
+        textViewPan1 = (TextView)findViewById(R.id.textViewPan1);
+
+        textViewPan2 = (TextView)findViewById(R.id.textViewPan2);
+
+        textViewPan3 = (TextView)findViewById(R.id.textViewPan3);
+
+        textViewPan4 = (TextView)findViewById(R.id.textViewPan4);
+
+        textViewPan5 = (TextView)findViewById(R.id.textViewPan5);
+
+
         layout_joystick = (RelativeLayout)findViewById(R.id.layout_joystick_move);
+
+        layout_joystickPantilt = (RelativeLayout)findViewById(R.id.layout_joystick_pantilt);
 
 
         js = new JoyStickClass(getApplicationContext()
 
                 , layout_joystick, R.drawable.joysticksmall);
+
+        jsPanTilt = new JoystickClassPanTilt(getApplicationContext()
+
+                , layout_joystickPantilt, R.drawable.joysticksmall);
+
+
 
         js.setStickSize(150, 150);
 
@@ -64,6 +115,108 @@ public class CamerActivity extends AppCompatActivity {
         js.setOffset(50);//skjusterara lilla cirkel
 
         js.setMinimumDistance(0);
+
+
+        jsPanTilt.setStickSize(150, 150);
+
+        jsPanTilt.setLayoutSize(300, 300);//bra måt :D. seekbar rotation 270 == vertical seekbar :D
+
+        jsPanTilt.setLayoutAlpha(200);
+
+        jsPanTilt.setStickAlpha(100);
+
+        jsPanTilt.setOffset(50);//skjusterara lilla cirkel
+
+        jsPanTilt.setMinimumDistance(0);
+
+
+
+
+        layout_joystickPantilt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                jsPanTilt.drawStick(event);
+
+                if (event.getAction() == MotionEvent.ACTION_DOWN
+                        || event.getAction() == MotionEvent.ACTION_MOVE) {
+
+
+                    textViewPan1.setText("X : " + String.valueOf(jsPanTilt.getspeedX()));
+
+                    textViewPan2.setText("Y : " + String.valueOf(jsPanTilt.getspeedY()));
+
+                    textViewPan3.setText("Angle : " + String.valueOf(jsPanTilt.getAngle()));
+                    textViewPan4.setText("Distance : " + String.valueOf(jsPanTilt.getDistance()));
+
+                    // Log.d("distans",String.valueOf(js.getDistance()));
+                //    new SendMessage().execute(String.valueOf(jsPanTilt.getspeedX()) + "," + String.valueOf(jsPanTilt.getspeedY()));
+
+
+                    int direction = jsPanTilt.get4Direction();
+
+
+                    if (direction == JoystickClassPanTilt.STICK_UP) {
+
+                        textViewPan5.setText("Direction : Up");
+
+
+                    } else if (direction == JoystickClassPanTilt.STICK_UPRIGHT) {
+
+                        textViewPan5.setText("Direction : Up Right");
+
+                    } else if (direction == JoyStickClass.STICK_RIGHT) {
+                        textViewPan5.setText("Direction : Right");
+
+                    } else if (direction == JoystickClassPanTilt.STICK_DOWNRIGHT) {
+
+                        textViewPan5.setText("Direction : Down Right");
+
+                    } else if (direction == JoystickClassPanTilt.STICK_DOWN) {
+
+                        textViewPan5.setText("Direction : Down");
+
+                    } else if (direction == JoystickClassPanTilt.STICK_DOWNLEFT) {
+
+                        textView5.setText("Direction : Down Left");
+
+                    } else if (direction == JoystickClassPanTilt.STICK_LEFT) {
+
+                        textViewPan5.setText("Direction : Left");
+                    } else if (direction == JoystickClassPanTilt.STICK_UPLEFT) {
+
+                        textViewPan5.setText("Direction : Up Left");
+
+                    } else if (direction == JoystickClassPanTilt.STICK_NONE) {
+
+                        textViewPan5.setText("Direction : Center");
+
+                    }
+
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+
+                    textViewPan1.setText("X :");
+
+                    textViewPan2.setText("Y :");
+
+
+                    textViewPan3.setText("Angle :");
+
+                    textViewPan4.setText("Distance :");
+
+                    textViewPan5.setText("Direction :");
+
+                }
+
+
+                return true;
+            }
+        });
+
+
+
+
+
 
 
         layout_joystick.setOnTouchListener(new View.OnTouchListener() {
@@ -174,5 +327,32 @@ public class CamerActivity extends AppCompatActivity {
                 (ip >> 24 & 0xff));
 
         return ipString;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        mp.setDisplay(holder);
+        try {
+            mp.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mediaPlayer) {
+                    mp.start();
+                }
+            });
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
     }
 }
